5f111872be96658f185fac8f224d20f066b4ea02
VaishnaviBunga <vaishnavibunga09@gmail.com>
2025-05-14 23:42:30 +0530

    update

---
+++ a9/script.js
@@ -0,0 +1,10 @@
+let fruits=["apple","orange","mango","banana"];
+let result=fruits.filter((value) => orange);
+document.getElementById(res).value=+result;
+
+let result=fruits.filter((value) => apple);
+console.log(result);
+let result=fruits.filter((value) => mango);
+console.log(result);
+let result=fruits.filter((value) => banana);
+console.log(result);
