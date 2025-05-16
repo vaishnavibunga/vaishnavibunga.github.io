38bef5d05d51fb54147a153964fec7579f58250d
VaishnaviBunga <vaishnavibunga09@gmail.com>
2025-05-16 19:42:29 +0530

    update

---
+++ a11/script.js
@@ -0,0 +1,6 @@
+const products=[
+    {id:1,name:apple ,price:3},
+    {id:2,name:orange ,price:5},
+    {id:3,name:mangp ,price:9},
+]
+     products.forEach((x) => console.log(x.name));
---
+++ a12/index.js
@@ -0,0 +1,7 @@
+const products=[
+    {id:1,name:apple ,price:3},
+    {id:2,name:orange ,price:5},
+    {id:3,name:mangp ,price:9},
+]
+    const new= products.map((x) => x.name+5);
+console.log(new);
