1f7304b9c716a37f80a036f10a5cc8b50134e236
VaishnaviBunga <vaishnavibunga09@gmail.com>
2025-05-16 19:47:46 +0530

    update

--- a12/index.js
+++ a12/index.js
@@ -3,5 +3,5 @@ const products=[
     {id:2,name:orange ,price:5},
     {id:3,name:mangp ,price:9},
 ]
-    const new= products.map((x) => x.name+5);
-console.log(new);
+    const element= products.map((x) => x.name+5);
+console.log(element);
